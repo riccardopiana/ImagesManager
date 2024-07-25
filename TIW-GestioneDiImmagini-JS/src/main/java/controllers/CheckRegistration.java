423600fad70e1c0a9ae341e26c034e33550b683f
riccardopiana <riccardo.pianalto@mail.polimi.it>
2024-07-25 17:24:00 +0200

    New folder for JS

---
+++ TIW-GestioneDiImmagini-JS/src/main/java/controllers/CheckRegistration.java
@@ -0,0 +1,110 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import beans.User;
+import dao.UserDAO;
+import utils.ConnectionHandler;
+
+@WebServlet("/CheckRegistration")
+public class CheckRegistration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+	private TemplateEngine templateEngine;
+
+	public CheckRegistration() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = null;
+		String surname = null;
+		String email = null;
+		String password = null;
+		String ripPassword = null;
+		try {
+			name = StringEscapeUtils.escapeJava(request.getParameter("name"));
+			surname = StringEscapeUtils.escapeJava(request.getParameter("surname"));
+			email = StringEscapeUtils.escapeJava(request.getParameter("email"));
+			password = StringEscapeUtils.escapeJava(request.getParameter("password"));
+			ripPassword = StringEscapeUtils.escapeJava(request.getParameter("ripPassword"));
+			if (name == null || surname == null|| email == null || password == null || ripPassword == null|| name.isEmpty() || surname.isEmpty() ||email.isEmpty() || password.isEmpty() || ripPassword.isEmpty()) {
+				throw new Exception("Missing or empty credential value");
+			}
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credential value");
+			return;
+		}
+
+		UserDAO userDao = new UserDAO(connection);
+		User user = null;
+		try {
+			user = userDao.checkCredentials(email, password);
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not Possible to check credentials");
+			return;
+		}
+
+		String path;
+		if (user != null) {
+			ServletContext servletContext = getServletContext();
+			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+			ctx.setVariable("registrationMsg", "User already exist");
+			path = "/index.html";
+			templateEngine.process(path, ctx, response.getWriter());
+		} else {
+			String message = null;
+			if (!password.equals(ripPassword)) {
+				message = "Password not equals";
+			}else if (!email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+				message = "Email adress not valid";
+			}else {
+				try {
+					userDao.addUser(email, password, name, surname);
+					message = "User correctly registered";
+				} catch (SQLException e) {
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to register user");
+				return;
+				}
+			}
+			ServletContext servletContext = getServletContext();
+			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+			ctx.setVariable("registrationMsg", message);
+			path = "/index.html";
+			templateEngine.process(path, ctx, response.getWriter());
+			}
+	}
+
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
